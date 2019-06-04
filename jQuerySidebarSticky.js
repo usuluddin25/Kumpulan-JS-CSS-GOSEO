bb779ce9a1b2eccbb37b5d1bf20bf40f9623dbd6
usuluddin25 <45456201+usuluddin25@users.noreply.github.com>
2019-06-04 12:26:56 +0700

    Create jQuerySidebarSticky.js

---
+++ jQuerySidebarSticky.js
@@ -0,0 +1,4 @@
+
+//<![CDATA[
+jQuery(document).ready(function(){var i=jQuery(window).width();function e(){jQuery("#sidebar-sticky, .little-side-winder").stick_in_parent({parent:"#wrapper",offset_top:70})}i<768?jQuery("#sidebar-sticky").trigger("sticky_kit:detach"):e(),jQuery(window).resize(function(){(i=jQuery(window).width())<768?jQuery("#sidebar-sticky, .little-side-winder").trigger("sticky_kit:detach"):e()})});
+//]]>
