d3e34a176cbbc9c1b0e37c20cbe019492e437ef4
usuluddin25 <45456201+usuluddin25@users.noreply.github.com>
2019-06-04 12:18:14 +0700

    Create ScrollTop.js

---
+++ ScrollTop.js
@@ -0,0 +1,11 @@
+$(window).scroll(function() {
+if($(this).scrollTop() &gt; 200) {
+    $(&#39;#back-to-top&#39;).fadeIn();
+} else {
+    $(&#39;#back-to-top&#39;).fadeOut();
+}
+});
+$(&#39;#back-to-top&#39;).hide().click(function() {
+$(&#39;html, body&#39;).animate({scrollTop:0}, 1000);
+return false;
+});
